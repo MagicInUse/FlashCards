d82ac3f7436b2ffa8d418f8b1fce7fda3e51031e
MagicInUse <jacob.norr@live.com>
2024-12-14 11:59:56 -0700

    Studied previous modules and challenges for a better setup and remembering npm packages used. localhost will run with npm run start:dev!

--- server/src/config/connections.js
+++ server/src/config/connections.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bootcamp');
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/flashcards');
     console.log('Database connected.');
     if (mongoose.connection.host) {console.log(mongoose.connection.host)};
     return mongoose.connection;
